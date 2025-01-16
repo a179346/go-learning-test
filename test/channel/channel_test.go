7c5572dc03750ed9fe591785da91c4f055779230
Robert Kuo <r122164152@gmail.com>
2025-01-16 15:32:04 +0800

    add close(channel)

--- test/channel/channel_test.go
+++ test/channel/channel_test.go
@@ -55,4 +55,43 @@ func TestChannel(t *testing.T) {
 			t.Errorf("v:%v , expected:%v", v, expected)
 		}
 	})
+	t.Run("`close(channel)` can stop multiple running go routine", func(t *testing.T) {
+		t.Parallel()
+
+		stop := make(chan struct{})
+		v := 0
+
+		go func() {
+			for {
+				select {
+				case <-stop:
+					return
+
+				case <-time.After(50 * time.Millisecond):
+					v += 1
+				}
+			}
+		}()
+
+		go func() {
+			for {
+				select {
+				case <-stop:
+					return
+
+				case <-time.After(130 * time.Millisecond):
+					v += 1
+				}
+			}
+		}()
+
+		time.Sleep(170 * time.Millisecond)
+		close(stop)
+		time.Sleep(300 * time.Millisecond)
+
+		expected := 4
+		if v != expected {
+			t.Errorf("v:%v , expected:%v", v, expected)
+		}
+	})
 }
